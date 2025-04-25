1c1cc3bc87785499d366c7b828761df63ac22e7a
Maria Treier <mariatraier@gmail.com>
2025-04-25 22:56:30 +0200

    feat: complete hash, streams

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const path = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  const stream = createReadStream(path);
+
+  stream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on('end', () => {
+    const result = hash.digest('hex');
+    console.log(result);
+    stream.close();
+  });
 };
 
 await calculateHash();
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileToReadPath = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  const stream = createReadStream(fileToReadPath, 'utf-8');
+
+  stream.on('data', (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  stream.on('end', () => {
+    stream.close();
+  })
 };
 
 await read();
